6a22cadbd0b563bd57855b4f1252f299bfe197ab
Dajdalos <34072803+Dajdalos@users.noreply.github.com>
2024-03-22 00:15:50 +0100

    portfolio

---
+++ assets/js/title.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".text", {
+    strings: ["Junior Frontend Developer", "Web Developer"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+  });
+});
